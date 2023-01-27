6ab2a9ba4c26f885ae0683696c5e2598e6beb5fd
vÅux <yvux.0511@gmail.com>
2023-01-27 22:35:11 +0700

    add file

---
+++ main.js
@@ -0,0 +1,9 @@
+//Đối tượng Validator
+function Validator(option) {
+    console.log(option);
+}
+
+//Định nghĩa các rules
+Validator.isRequired = function () {};
+
+Validator.isEmail = function () {};
